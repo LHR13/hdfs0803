23939a13e238453d1dfae07b9f7b213a340dcd2f
LHR13 <1309184697@qq.com>
2020-08-05 17:48:03 +0800

    08051

--- src/main/java/com/atguigu/hdfsclient/WordCount/WCDriver.java
+++ src/main/java/com/atguigu/hdfsclient/WordCount/WCDriver.java
@@ -1,19 +1,43 @@
 package com.atguigu.hdfsclient.WordCount;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
 
-public class WCDriver extends Mapper<LongWritable, Text, Text, IntWritable> {
-    @Override
-    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String line = value.toString();
-        String[] words = line.split(" ");
-        for (String word : words) {
-            context.write(new Text(word), new IntWritable(1));
-        }
+public class WCDriver  {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        //1.获取一个job实例
+        Job job = Job.getInstance(new Configuration());
+
+        //2.设置类路径（classpath）
+        job.setJarByClass(WCDriver.class);
+
+        //3.设置Mapper和reducer
+        job.setMapperClass(WCMapper.class);
+        job.setReducerClass(WCReducer.class);
+
+        //4.设置Mapper和Reducer输出的类型
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        //5.设置输入输出源
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        //6.提交job
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
+
+
 }
--- src/main/java/com/atguigu/hdfsclient/WordCount/WCMapper.java
+++ src/main/java/com/atguigu/hdfsclient/WordCount/WCMapper.java
@@ -1,4 +1,27 @@
 package com.atguigu.hdfsclient.WordCount;
 
-public class WCMapper {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class WCMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    private Text word = new Text();
+    private IntWritable one = new IntWritable(1);
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        String[] words = line.split(" ");
+        for (String word : words) {
+            //context.write(new Text(word), new IntWritable(1));
+            //最好不要这样每次都new一个新对象
+
+            this.word.set(word);
+            context.write(this.word, this.one);
+        }
+    }
 }
